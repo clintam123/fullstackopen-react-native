c4431fc7e6496c46f533e62e64870a94135d006d
Đỗ Đức Tâm <nguyenhanh1074@gmail.com>
2021-12-26 17:44:33 +0700

    Exercise 10.3

---
+++ rate-repository-app/src/components/RepositoryItem.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const properties = [
+  "Id",
+  "Full name",
+  "Description",
+  "Language",
+  "Stars",
+  "Forks",
+  "Reviews",
+  "Rating",
+  "OwnerAvatarUrl",
+];
+
+const RepositoryItem = ({ item }) => {
+  return (
+    <>
+      {Object.values(item).map((e, index) => (
+        <Text>
+          {properties[index]}: {e}
+        </Text>
+      ))}
+    </>
+  );
+};
+
+export default RepositoryItem;
