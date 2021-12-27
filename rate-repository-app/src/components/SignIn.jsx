36bc9b7bf977e593bb21099c2228ab4308d425e7
Đỗ Đức Tâm <nguyenhanh1074@gmail.com>
2021-12-27 14:04:22 +0700

    10.8: the sign-in form

--- rate-repository-app/src/components/SignIn.jsx
+++ rate-repository-app/src/components/SignIn.jsx
@@ -1,9 +1,38 @@
 import React from "react";
+import { Formik } from "formik";
+import { View, Button, StyleSheet } from "react-native";
 
-import Text from "./Text";
+import FormikTextInput from "./FormikTextInput";
+
+const SignInForm = ({ onSubmit }) => {
+  return (
+    <View>
+      <FormikTextInput name="username" placeholder="Username" />
+      <FormikTextInput
+        name="password"
+        placeholder="Password"
+        secureTextExtry={true}
+      />
+      <Button onPress={onSubmit} title="Sign In" />
+    </View>
+  );
+};
 
 const SignIn = () => {
-  return <Text>The sign-in view</Text>;
+  const initialValues = {
+    username: "",
+    password: "",
+  };
+
+  const onSubmit = (values) => {
+    console.log(values);
+  };
+
+  return (
+    <Formik initialValues={initialValues} onSubmit={onSubmit}>
+      {({ handleSubmit }) => <SignInForm onSubmit={handleSubmit} />}
+    </Formik>
+  );
 };
 
 export default SignIn;
