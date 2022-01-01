38c38af8b724510416bece572bdbc1e217b6ebb5
Đỗ Đức Tâm <nguyenhanh1074@gmail.com>
2022-01-02 01:58:43 +0700

    Exercise 10.23: sorting the reviewed repositories list

--- rate-repository-app/src/components/RepositoryList.jsx
+++ rate-repository-app/src/components/RepositoryList.jsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import { FlatList, View, StyleSheet, Pressable } from "react-native";
 import { useHistory } from "react-router-native";
 
 import RepositoryItem from "./RepositoryItem";
+import SortingMenu from "./SortingMenu";
 import useRepositories from "../hooks/useRepositories";
 
 const styles = StyleSheet.create({
@@ -13,7 +14,11 @@ const styles = StyleSheet.create({
 
 const ItemSeparator = () => <View style={styles.separator} />;
 
-export const RepositoryListContainer = ({ repositories }) => {
+export const RepositoryListContainer = ({
+  repositories,
+  setSortingCriteria,
+  sorting,
+}) => {
   const repositoryNodes = repositories
     ? repositories.edges.map((edge) => edge.node)
     : [];
@@ -23,6 +28,12 @@ export const RepositoryListContainer = ({ repositories }) => {
     <FlatList
       data={repositoryNodes}
       keyExtractor={({ id }) => id}
+      ListHeaderComponent={() => (
+        <SortingMenu
+          setSortingCriteria={setSortingCriteria}
+          sorting={sorting}
+        />
+      )}
       renderItem={({ item }) => (
         <Pressable onPress={() => history.push(`/${item.id}`)}>
           <RepositoryItem repository={item} />
@@ -34,9 +45,16 @@ export const RepositoryListContainer = ({ repositories }) => {
 };
 
 const RepositoryList = () => {
-  const { repositories } = useRepositories();
+  const [sorting, setSorting] = useState("latest_repos");
+  const { repositories } = useRepositories(sorting);
 
-  return <RepositoryListContainer repositories={repositories} />;
+  return (
+    <RepositoryListContainer
+      repositories={repositories}
+      setSortingCriteria={setSorting}
+      sorting={sorting}
+    />
+  );
 };
 
 export default RepositoryList;
--- rate-repository-app/src/hooks/useRepositories.js
+++ rate-repository-app/src/hooks/useRepositories.js
@@ -2,9 +2,16 @@ import { useQuery } from "@apollo/client";
 
 import { GET_REPOSITORIES } from "../graphql/queries";
 
-const useRepositories = () => {
+const sortCriteriaOptions = {
+  latest_repos: { orderBy: "CREATED_AT", orderDirection: "DESC" },
+  highest_rated_repos: { orderBy: "RATING_AVERAGE", orderDirection: "DESC" },
+  lowest_rated_repos: { orderBy: "RATING_AVERAGE", orderDirection: "ASC" },
+};
+
+const useRepositories = (sortCriteria) => {
   const { data, ...result } = useQuery(GET_REPOSITORIES, {
     fetchPolicy: "cache-and-network",
+    variables: sortCriteriaOptions[sortCriteria],
   });
 
   return { repositories: data ? data.repositories : undefined, ...result };
