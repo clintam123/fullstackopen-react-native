60fd88f64cd47d1636580d6bd63d8b88490f03b6
Đỗ Đức Tâm <nguyenhanh1074@gmail.com>
2022-01-01 19:31:55 +0700

    Fix formatDate

--- rate-repository-app/src/utils/formatDate.js
+++ rate-repository-app/src/utils/formatDate.js
@@ -1,8 +1,10 @@
 const formatDate = (dateString) => {
   const date = new Date(dateString);
-  return [date.getUTCDate(), date.getUTCMonth(), date.getUTCFullYear()].join(
-    "."
-  );
+  return [
+    date.getUTCDate(),
+    date.getUTCMonth() + 1,
+    date.getUTCFullYear(),
+  ].join(".");
 };
 
 export default formatDate;
